1e9260bbc90ab58d3cbf981388da493695f766aa
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-28 23:42:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,16 +16,15 @@ class ApplicationController < Sinatra::Base
 		erb :signup
 	end
 
-	post "/signup" do
-		#your code here!
-		user = User.new(username: params[:username], password: params[:password])
-
-		if user.save
-			redirect to "/login"
-		else
-			redirect to "/failure"
-		end
-	end
+  post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+ 
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+  end
 
 	get "/login" do
 		erb :login
