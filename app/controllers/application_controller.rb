68a4b0ece12763dfdbb8e6a15ebf209f78e0402a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-28 23:42:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,11 +30,9 @@ class ApplicationController < Sinatra::Base
 		erb :login
 	end
 
-	post "/login" do
-		#your code here!
+  post "/login" do
     user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if user
       redirect "/success"
     else
       redirect "/failure"
